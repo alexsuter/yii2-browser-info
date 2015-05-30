4c835779f5e9fbd7d4a4468f657c5e111e2cbbf7
alexander.suter <alexander.suter.ch@gmail.com>
2015-05-30 11:14:23 +0200

    Added new tests

--- tests/BrowserInfoBrowscapTest.php
+++ tests/BrowserInfoBrowscapTest.php
@@ -10,10 +10,29 @@ class BrowserInfoBrowscapTest extends \PHPUnit_Framework_TestCase {
         $this->assertEquals(null, $browserName);
     }
     
+    public function testGetBrowserNameReturnOperaWhenUserAgentIsOpera() {
+        $_SERVER['HTTP_USER_AGENT'] = 'Opera/9.63 (Macintosh; Intel Mac OS X; U; en) Presto/2.1.1';
+        $testee = new BrowserInfoBrowscap(['cacheDir' => __DIR__ . '/fixtures/browscap']);
+        $browserName = $testee->getBrowserName();
+        $this->assertEquals('Opera', $browserName);
+    }
+    
     public function testIsBotReturnFalseWhenNoCacheIsAvailable() {
         $testee = new BrowserInfoBrowscap();
         $isBot = $testee->isBot();
         $this->assertFalse($isBot);
+    }
+    
+    public function testIsBotReturnTrueWhenUserAgentIsGoogleBot() {
+        $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
+        $testee = new BrowserInfoBrowscap(['cacheDir' => __DIR__ . '/fixtures/browscap']);
+        $this->assertTrue($testee->isBot());
+    }
+    
+    public function testIsBotReturnFalseWhenUserAgentIsOperaBrowser() {
+        $_SERVER['HTTP_USER_AGENT'] = 'Opera/9.63 (Macintosh; Intel Mac OS X; U; en) Presto/2.1.1';
+        $testee = new BrowserInfoBrowscap(['cacheDir' => __DIR__ . '/fixtures/browscap']);
+        $this->assertFalse($testee->isBot());
     }
 
 }
